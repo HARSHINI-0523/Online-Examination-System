3d11e0bd77972163e838ac5f579540ee86f8b84e
HARSHINI-0523 <gopisettyharshini23@gmail.com>
2025-03-31 19:24:36 +0530

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,55 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import path from 'path';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 
-import events from 'events';
+const events = require("events");
 events.EventEmitter.defaultMaxListeners = 20;
 
 dotenv.config();
 
 // Import Routes
-import userRoutes from './routes/userRoutes.js';
-import examRoutes from './routes/examRoutes.js';
-import teacherRoutes from './routes/teacherRoutes.js';
-import classroomRoutes from './routes/classroomRoutes.js';
+const userRoutes = require("./routes/userRoutes");
+const examRoutes = require("./routes/examRoutes");
+const teacherRoutes = require("./routes/teacherRoutes");
+const classroomRoutes = require("./routes/classroomRoutes");
 
 const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:5173', // Frontend URL
+  origin: "http://localhost:5173", // Frontend URL
   credentials: true, // Allow sending cookies
 }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/user', userRoutes);
-app.use('/api/exams', examRoutes);
-app.use('/api/teacher', teacherRoutes);
-app.use('/api/classrooms', classroomRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/exams", examRoutes);
+app.use("/api/teacher", teacherRoutes);
+app.use("/api/classrooms", classroomRoutes);
 
-const __dirname = path.resolve();
+//const __dirname = path.resolve();
 
 // Serve Frontend in Production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
   });
